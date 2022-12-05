60a6f09f59200fedc5bce8c6c79ef31aa12c40c7
joao-lages <joaolages.dev@gmail.com>
2022-12-05 17:54:50 -0300

    First commit

---
+++ src/br/com/basic/Operators.java
@@ -0,0 +1,34 @@
+package br.com.basic;
+
+public class Operators {
+
+    public static void main(String[] args) {
+
+        int side = 2;
+        float pi = 3.1415926f;
+        String text = "Text";
+        side = 3;
+
+        System.out.println(side);
+
+        int sum = 2 + 2;
+        System.out.println(sum);
+
+        int multiplication = 6 * 7;
+        System.out.println(multiplication);
+
+        int subtraction = 7 - 2;
+        System.out.println(subtraction);
+
+        int module = 25 % 4;
+        System.out.println(module);
+
+        boolean result = 5 >= 3;
+        System.out.println(result);
+
+        boolean a = true;
+        boolean b = false;
+        result = a|| b;
+        System.out.println(result);
+    }
+}

be93ebe071895eb9b5dfecf5f50f4ba5ded18286
joao-lages <joaolages.dev@gmail.com>
2022-12-05 18:09:21 -0300

    Class: increment and decrement

---
+++ src/br/com/basic/IncrementDecrement.java
@@ -0,0 +1,25 @@
+package br.com.basic;
+
+public class IncrementDecrement {
+
+    public static void main(String[] args) {
+
+        int i = 5;
+        System.out.println(i);
+
+        int aa = i++;
+        System.out.println(aa);
+        System.out.println(i);
+
+        int bb = ++i;
+        System.out.println(bb);
+        System.out.println(i);
+
+        int c = i--;
+        System.out.println(c);
+        System.out.println(i);
+
+        int d = --i;
+        System.out.println(d);
+    }
+}

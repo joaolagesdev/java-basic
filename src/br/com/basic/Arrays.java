7dd8e29a91e3003292926418316fbd6885312a3d
joao-lages <joaolages.dev@gmail.com>
2022-12-06 22:09:55 -0300

    Class: Arrays

---
+++ src/br/com/basic/Arrays.java
@@ -0,0 +1,17 @@
+package br.com.basic;
+
+public class Arrays {
+    public static void main(String[] args) {
+        int[] numbers = {23, 46, 2, 125};
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Position: " + i + " Value: " + numbers[i]);
+        }
+
+        System.out.println("--------------------------------");
+
+        for(int number : numbers) {
+            System.out.println("Value: " + number);
+        }
+    }
+}

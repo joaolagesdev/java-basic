ccdb5345dadcaa06876a8c91f0c0a70ee7b2d801
joao-lages <joaolages.dev@gmail.com>
2022-12-14 18:32:16 -0300

    Exercice 7

--- src/br/com/basic/exercices/Method.java
+++ src/br/com/basic/exercices/Method.java
@@ -1,9 +1,5 @@
 package br.com.basic.exercices;
 
-import java.util.OptionalInt;
-
-import static java.util.Arrays.stream;
-
 public class Method {
     public static void main(String[] args) {
         int[] numbers = {0, 1, 4, 6, 9, 5, 10};
